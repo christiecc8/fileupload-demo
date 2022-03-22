d0c194abe6676f4a65ab1914411e1d3899afc5a4
christiecc8 <christie.cc8@gmail.com>
2022-03-22 11:46:00 -0400

    Posting up

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require('fs');
+
 const pinataApiKey='ab7a2d0d4fe81f7f21ce'
 const pinataSecretApiKey='f7798043ec32dcc7c8f7b5870651579b2a76f855e556eabb2689c33cc51093bc'
 
@@ -11,7 +13,12 @@ app.get('/testAPI', function(request, response) {
 });
 
 app.post('/uploadFile', function(request, response) {
-    console.log(request.json())
+  response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+  response.send("Received");
+  fs.readFile(request.files.file.path, function(err, data) {
+     console.log(data)
+  });
 });
 
 app.listen(process.env.PORT || 9000);
