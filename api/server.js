97b243f570254b5f2610cb2fbf624f471f9a876b
christiecc8 <christie.cc8@gmail.com>
2022-03-23 17:05:13 -0400

    Finally works with multiparty

--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const fs = require('fs');
-const axios = require('axios');
 const FormData = require('form-data');
 const bodyParser = require('body-parser');
-let busboy = require('busboy');
-const { Readable } = require('stream');
 const pinataSDK = require('@pinata/sdk');
+const multiparty = require('multiparty');
+var util = require('util');
 
 const url = 'https://api.pinata.cloud/pinning/pinFileToIPFS'
 const pinataApiKey='ab7a2d0d4fe81f7f21ce'
@@ -22,90 +21,54 @@ server.get('/testAPI', function(request, response) {
   response.send("API is up and working!");
 });
 
-// server.post('/uploadFile', function (request, response) {
-//   response.header("Access-Control-Allow-Origin", "*");
-// 	response.header("Access-Control-Allow-Headers", "X-Requested-With");
-  
-//   let formData = new FormData();
-
-//   const bb = busboy({ headers: request.headers });
-//   const buf = [];
-  
-//   bb.on('file', (name, file, info) => {
-//     file.on("data", (d) => {
-//       buf.push(d)
-//     }).on("end", async () => {
-//       let data = Buffer.concat(buf);
-//       console.log("This is the buffer");
-//       console.log(data);
-//       const readStream = new Readable({
-//         read() {
-//           this.push(data)
-//         }
-//       })
-//       console.log("This is the read stream:")
-//       console.log(readStream);
-//       formData.append('file', readStream, name);
-//     });
-//   });
-//   request.pipe(bb);
-
-//   axios.post(url,
-//     formData, 
-//     {
-//       headers: {
-//           'Content-Type': `multipart/form-data; boundary= ${formData._boundary}`,
-//           'pinata_api_key': pinataApiKey,
-//           'pinata_secret_api_key': pinataSecretApiKey
-//       }
-//     }
-//   ).then(function(response) {
-//     console.log("success");
-//     console.log(response);
-//   }).catch(function(error){
-//     throw new Error(error);
-//     console.log("This is the form data")
-//     console.log(formData)
-//   });
-// });
-
 server.post('/uploadFile', function (request, response) {
   response.header("Access-Control-Allow-Origin", "*");
 	response.header("Access-Control-Allow-Headers", "X-Requested-With");
   
-  const bb = busboy({ headers: request.headers });
-  const buf = [];
-  bb.on('file', (name, file, info) => {
-      file.on('data', (data) => {
-        buf.push(data);
-      })
-      file.on('end', () => {
-        const readStream = createReadableStream(buf);
-        console.log(readStream);
-    pinata.pinFileToIPFS(readStream).then((result) => {
-      //handle results here
-      console.log(result);
-  }).catch((err) => {
-      //handle error here
-      console.log(err);
+  var form = new multiparty.Form();
+  form.on('error', function(err) {
+    console.log('Error parsing form: ' + err.stack);
   });
+  
+  // Parts are emitted when parsing the form
+  form.on('part', function(part) {
+    // You *must* act on the part by reading it
+    // NOTE: if you want to ignore it, just call "part.resume()"
+  
+    if (part.filename === undefined) {
+      // filename is not defined when this is a field and not a file
+      console.log('got field named ' + part.name);
+      // ignore field's content
+      part.resume();
+    }
+  
+    if (part.filename !== undefined) {
+      // filename is defined when this is a file
+      console.log('got file named ' + part.name);
+      pinata.pinFileToIPFS(part).then((result) => {
+          //handle results here
+          console.log(result);
+      }).catch((err) => {
+          //handle error here
+          console.log(err);
       });
+      // ignore file's content here
+      part.resume();
+    }
+  
+    part.on('error', function(err) {
+      // decide what to do
+    });
   });
-  request.pipe(bb);
-    
+  
+  // Close emitted after form parsed
+  form.on('close', function() {
+    console.log('Upload completed!');
+  });
+  
+  // Parse req
+  form.parse(request);
 });
-
-const createReadableStream = (buffer) => {
-  const readableInstanceStream = new Readable({
-      read() {
-          for (const bytes of buffer) {
-              this.push(bytes);
-          }
-          this.push(null);
-      },
-  });
-  return readableInstanceStream;
-};
 
 server.post('/uploadFileTesting', function (request, response) {
   let data = new FormData();
