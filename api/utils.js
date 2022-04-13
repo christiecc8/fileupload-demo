749619184a39f4163a5e78488881f813f9df115b
christiecc8 <christie.cc8@gmail.com>
2022-04-13 15:35:32 -0400

    Troubleshooting pinning

--- api/utils.js
+++ api/utils.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config()
 
 const pinataApiKey = process.env.PINATA_API_KEY;
 const pinataSecretApiKey = process.env.PINATA_API_SECRET_KEY;
@@ -8,6 +9,8 @@ const pinata = pinataSDK(pinataApiKey, pinataSecretApiKey);
 
 module.exports = {
     uploadImage:function(readStream) {
+        console.log("test key: " + pinataApiKey);
+        console.log("test secret: " + pinataSecretApiKey);
         let promise = new Promise(function(resolve, reject) { 
             resolve(pinata.pinFileToIPFS(readStream));
         });
